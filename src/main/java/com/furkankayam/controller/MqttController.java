45546eeea05243ec9253ef404f1adb4b7d63d10d
Furkan Kaya <furkan.36kaya@gmail.com>
2023-12-11 19:13:07 +0300

    fix: fix cross origin

--- src/main/java/com/furkankayam/controller/MqttController.java
+++ src/main/java/com/furkankayam/controller/MqttController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MqttController {
 
     private final MessageService messageService;
